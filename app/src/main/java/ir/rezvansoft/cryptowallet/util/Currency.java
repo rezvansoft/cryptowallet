c8bb99378c5bda8f508d580684e7b78caf174c4d
rezvansoft <rezvansoft6137@gmail.com>
2021-01-26 18:42:39 +0330

    Add files via upload

---
+++ app/src/main/java/ir/rezvansoft/cryptowallet/util/Currency.java
@@ -0,0 +1,32 @@
+package ir.rezvansoft.cryptowallet.util;
+
+/**
+ * Created by Sreejith on 22-Nov-17.
+ */
+
+public class Currency {
+    public static String EXTRA_DATA_CURRENCY_CODE="currency_code";
+    public static final String USD="USD";
+    public static final String INR="INR";
+    public static final String EUR="EUR";
+    public static final String CAD="CAD";
+    public static final String NZD="NZD";
+    public static final String AUD="AUD";
+    public static final String JPY="JPY";
+    public static final String GBP="GBP";
+    public static final String PLN="PLN";
+    public static final String RUB="RUB";
+    public static final String SGD="SGD";
+    public static final String HKD="HKD";
+    public static final String CHF="CHF";
+    public static final String LTC="LTC";
+    public static final String ZAR="ZAR";
+    public static final String KRW="KRW";
+    public static final String ILS="ILS";
+    public static final String SEK="SEK";
+    public static final String BRL="BRL";
+    public static final String NGN="NGN";
+    public static final String[] currencyArray={USD,INR,EUR,CAD,NZD,AUD,JPY,GBP,PLN,RUB};
+
+
+}
---
+++ app/src/main/java/ir/rezvansoft/cryptowallet/util/RecyclerviewSearchListener.java
@@ -0,0 +1,10 @@
+package ir.rezvansoft.cryptowallet.util;
+
+/**
+ * Created by Sreejith on 26-Nov-17.
+ */
+
+public interface RecyclerviewSearchListener {
+     void onSearch(String searchString);
+
+}
